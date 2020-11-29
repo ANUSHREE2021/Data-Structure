f6728f1395865da6d9d3d86ff9f316747e15c680
ANUSHREE2021 <75194291+ANUSHREE2021@users.noreply.github.com>
2020-11-28 23:01:57 -0800

    Add files via upload

---
+++ oddeven.c
@@ -0,0 +1,14 @@
+//To check whether a number is odd or even
+#include<stdio.h>
+main()
+{
+	int a;
+	printf("Enter the number\n");
+	scanf("%d",&a);
+	if(a%2==0)
+	 printf("Number is even\n");
+	else
+	 printf("Number is odd\n");
+
+}
+
