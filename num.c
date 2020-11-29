1f70d2cc31a9d8c1e18ecdd32631fb7f4b22ff7d
ANUSHREE2021 <75194291+ANUSHREE2021@users.noreply.github.com>
2020-11-28 23:03:24 -0800

    Add files via upload

---
+++ num.c
@@ -0,0 +1,16 @@
+//To check whether a number is less than 100 or not.
+#include<stdio.h>
+main()
+{
+	int n;
+	printf("Enter the number");
+	scanf("%d",&n);
+	if(n<100)
+	{
+	 printf("Number is less than 100\n");
+	}
+	else
+	{
+	printf("Number is not less than 100\n");
+	}
+}
